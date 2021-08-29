e4e98593d75620e7c57b9a57f484b7d2d5a1a619
akash6280 <akashsingh6280@gmail.com>
2021-08-30 02:01:38 +0530

    REFACTOR:Replaced three getMaximum function by single generic function

--- src/com/bridgelabz/testmaximumproblem/TestMaximum.java
+++ src/com/bridgelabz/testmaximumproblem/TestMaximum.java
@@ -1,25 +1,23 @@
 package com.bridgelabz.testmaximumproblem;
 
 public class TestMaximum { 
-	public static void getMaximum(String firstWord, String secondWord, String thirdWord) {
-		if (firstWord.compareTo(secondWord) > 0) {
-			if (firstWord.compareTo(thirdWord) > 0) {
-				System.out.println(firstWord);
-			} 
-			else {
-				System.out.println(thirdWord);
-			}
+	public static <T extends Comparable<T>> void getMaximum(T first, T second, T third) {
+		if (first.compareTo(second) > 0) {
+			if (first.compareTo(third) > 0)
+				System.out.println(first);
+			else
+				System.out.println(third);
 		} 
 		else {
-			if (secondWord.compareTo(thirdWord) > 0) {
-				System.out.println(secondWord);
-			} 
-			else {
-				System.out.println(thirdWord);
-			}
+			if (second.compareTo(third) > 0) 
+				System.out.println(second);
+			else 
+				System.out.println(third);
 		}
 	}
 	public static void main(String[] args) {
+		getMaximum(31,1313,131);
+		getMaximum(31.0f,65.2f,63.8f);
 		getMaximum("Apple","Peach","Banana");
 		
 	}
